f55bb836b98526cf20565f74d34a88615afb42a9
Corentin <corentin@seventeeninfo.fr>
2024-01-05 23:07:11 +0100

    CGU

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import Nav from "../../components/general/nav";
 import Header from "../../components/general/header";
 
 export default function Home() {
--- src/pages/legals/cgu.js
+++ src/pages/legals/cgu.js
@@ -2,8 +2,71 @@ import React from "react";
 
 export default function Cgu() {
     return (
-        <div>
-            <p>Cgu</p>
-        </div>
+        <main>
+            <section className="page-padding">
+                <h1>Conditions Générales d'Utilisation du Réseau Wi-Fi</h1>
+                <div>
+                    <h2>1. Accès au réseau</h2>
+                    <p>L'accès à Internet est accessible aux utilisateurs habilités disposant d'un identifiant de
+                        connexion.</p>
+                    <p>En accédant à notre réseau Wi-Fi, vous acceptez de vous conformer à toutes les règles et
+                        politiques en vigueur. L'accès est fourni à des fins personnelles et non commerciales.</p>
+                    <p>De plus, l'utilisation par les mineurs de ce service s'effectue sous la seule responsabilité de
+                        leurs parents ou responsables légaux.</p>
+                </div>
+                <div>
+                    <h2>2. Utilisation responsable</h2>
+                    <p>Vous êtes responsable de votre comportement et de l'utilisation du réseau. Toute activité
+                        illégale, abusive ou contraire aux lois locales est strictement interdite.</p>
+                </div>
+                <div>
+                    <h2>3. Sécurité</h2>
+                    <p>Nous déclinons toute responsabilité quant à la sécurité de vos données lors de l'utilisation du
+                        réseau. Il est recommandé d'éviter les transactions sensibles et de sécuriser vos appareils.</p>
+                </div>
+                <div>
+                    <h2>4. Contenu</h2>
+                    <p>Nous ne sommes pas responsables du contenu accessible via le réseau. Vous êtes tenu de respecter
+                        les droits d'auteur et de ne pas accéder à des contenus illégaux ou offensants.</p>
+                </div>
+                <div>
+                    <h2>5. Interruption du service</h2>
+                    <p>Nous nous réservons le droit de suspendre ou d'interrompre l'accès au réseau à tout moment, sans
+                        préavis, pour des raisons de maintenance ou de sécurité.</p>
+                </div>
+                <div>
+                    <h2>6. Conditions d’utilisation générales</h2>
+                    <p>En utilisant ce service, l'utilisateur s'engage à :</p>
+                    <ul>
+                        <li>Ne pas diffamer, harceler ou violer les droits d'autrui</li>
+                        <li>Ne pas consulter des sites à caractère raciste, pédophile ou incitant à la haine et à la
+                            violence
+                        </li>
+                        <li>Ne pas diffuser ou permettre de télécharger tous les éléments contenant les logiciels ou
+                            autres éléments protégés par les droits de la propriété intellectuelle
+                        </li>
+                        <li>Ne pas créer une fausse identité</li>
+                        <li>Ne pas tenter d’obtenir un accès non autorisé à un service et/ou à une donnée et/ou à un
+                            fichier
+                        </li>
+                        <li>Ne pas commettre de délits et actes de piratage portant atteinte aux droits d’autrui et à la
+                            sécurité des personnes
+                        </li>
+                        <li>Ne pas adresser de message comprenant des propos injurieux, diffamatoires, obscènes,
+                            illicites ou portant atteinte à tout droit, notamment les droits à la personne humaine
+                        </li>
+                        <li>Ne pas perturber les services et /ou contenus et/ou données auxquels il accède.</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Responsabilité de SEVENTEEN INFORMATIQUE</h2>
+                    <p>L'utilisateur est le seul responsable de tout préjudice direct ou indirect, matériel ou
+                        immatériel causé à SEVENTEEN INFORMATIQUE et/ou des tiers du fait de son utilisation de ce
+                        service.</p>
+                </div>
+                <p>En utilisant notre réseau Wi-Fi, vous reconnaissez avoir lu, compris et accepté ces conditions
+                    générales d'utilisation. Nous nous réservons le droit de modifier ces conditions à tout moment.</p>
+            </section>
+        </main>
     );
 }
