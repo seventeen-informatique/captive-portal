0d9a3a59e43096611f99efb9de0a657f234dd88a
Corentin <corentin@seventeeninfo.fr>
2024-01-05 16:53:28 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/medias/logo/logo.svg';
+import './assets/css/main.css';
+
+const Nav = () => {
+    return (
+      <nav>
+          <div>
+              <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                  <img src={logo} className="App-logo" alt="logo"/>
+              </a>
+          </div>
+      </nav>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Nav/>
+    );
 }
 
 export default App;
